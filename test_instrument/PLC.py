6862ba99061a3610d2c89d0d022add893e0bd3a1
chun_lab notebook <zx511629@gmail.com>
2024-06-12 14:57:10 +0800

    test PLC

--- test_instrument/PLC.py
+++ test_instrument/PLC.py
@@ -1,9 +1,3 @@
-import sys
-import os
-instrument_module_path = 'C:/Users/Acer/battery_test_project/instrument.py'
-if instrument_module_path not in sys.path:
-    sys.path.append(instrument_module_path)
-
 import instrument
 import time
 
@@ -12,6 +6,6 @@ DVP_12SE = instrument.DVP_PLC('COM4', '12SE')
 time.sleep(1) #要確保rs232命令被發出
 M1183_state = DVP_12SE.M1183_output(b':01050C9F00004F\r\n')
 output_state = DVP_12SE.Y0_output(b':01050500FF00F6\r\n')
-time.sleep(5)
+time.sleep(3)
 output_state = DVP_12SE.Y0_output(b':010505000000F5\r\n')
 
