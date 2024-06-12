dbfc7b4782c55028f0a8513425af431ff64d6bfe
heieveryone <zx511629@gmail.com>
2024-06-12 14:00:21 +0800

    test PLC

---
+++ test_instrument/PLC.py
@@ -0,0 +1,10 @@
+import instrument
+import time
+
+DVP_12SE = instrument.DVP_PLC('COM3', '12SE')
+time.sleep(1) #要確保rs232命令被發出
+M1183_state = DVP_12SE.M1183_output(b':01050C9F00004F\r\n')
+output_state = DVP_12SE.Y0_output(b':01050500FF00F6\r\n')
+time.sleep(5)
+output_state = DVP_12SE.Y0_output(b':010505000000F5\r\n')
+
