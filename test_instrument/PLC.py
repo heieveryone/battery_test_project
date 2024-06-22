79c0e0b9d6df19b1d22915959e9b4d3ad8a9899c
chun_lab notebook <zx511629@gmail.com>
2024-06-22 15:35:46 +0800

    test

--- test_instrument/PLC.py
+++ test_instrument/PLC.py
@@ -6,6 +6,7 @@ DVP_12SE = instrument.DVP_PLC('COM4', '12SE')
 time.sleep(1) #要確保rs232命令被發出
 M1183_state = DVP_12SE.M1183_output(b':01050C9F00004F\r\n')
 output_state = DVP_12SE.Y0_output(b':01050500FF00F6\r\n')
-time.sleep(3)
+print(M1183_state, output_state)
+time.sleep(10)
 output_state = DVP_12SE.Y0_output(b':010505000000F5\r\n')
-
+print(M1183_state, output_state)
